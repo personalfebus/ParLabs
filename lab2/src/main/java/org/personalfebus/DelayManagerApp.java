a537c5c9091d2a1cade33cbaffcd6440e6668e8d
personalfebus <gleb89206535210@gmail.com>
2020-11-03 14:35:13 +0300

    Scripted auto-commit on change (2020-11-03 14:35:13) by gitwatch.sh

--- lab2/src/main/java/org/personalfebus/DelayManagerApp.java
+++ lab2/src/main/java/org/personalfebus/DelayManagerApp.java
@@ -6,7 +6,7 @@ import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class WordCountApp {
+public class DelayManagerApp {
 	public static void main(String[] args) throws Exception {
 		if (args.length != 2) {
 			System.err.println("Usage: WordCountApp <input path> <output path>");
