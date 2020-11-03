bc44e0eea1a86dd4bf85170bf5e4c71b2ee1c310
personalfebus <gleb89206535210@gmail.com>
2020-11-03 20:09:39 +0300

    Scripted auto-commit on change (2020-11-03 20:09:39) by gitwatch.sh

--- lab2/dolab.sh
+++ lab2/dolab.sh
@@ -1,6 +1,6 @@
 mvn package
 hadoop fs -rm -r output
-hadoop org.personalfebus.WordCountApp ONTIME_SAMPLE.csv output
+hadoop org.personalfebus.DelayManagerApp ONTIME_SAMPLE.csv output
 rm -r output
 hadoop fs -copyToLocal output
 
