69078fd4b4a86bdc77ec76f80d1a7d37d8bfe5fd
personalfebus <gleb89206535210@gmail.com>
2020-11-04 00:36:54 +0300

    Scripted auto-commit on change (2020-11-04 00:36:54) by gitwatch.sh

--- lab2/src/main/java/org/personalfebus/DelayManagerApp.java
+++ lab2/src/main/java/org/personalfebus/DelayManagerApp.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class DelayManagerApp {
