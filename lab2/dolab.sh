740cf5a93a0b0a8d40b9a16f42d28012b29b1d6f
personalfebus <gleb89206535210@gmail.com>
2020-11-03 20:08:37 +0300

    Scripted auto-commit on change (2020-11-03 20:08:37) by gitwatch.sh

--- lab2/dolab.sh
+++ lab2/dolab.sh
@@ -1,6 +1,6 @@
 mvn package
 hadoop fs -rm -r output
-hadoop org.personalfebus.WordCountApp warandpeace1.txt output
+hadoop org.personalfebus.WordCountApp ONTIME_SAMPLE.csv output
 rm -r output
 hadoop fs -copyToLocal output
 
