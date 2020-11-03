3ffe021a03eb5e027d7312434dca259807f394c0
personalfebus <gleb89206535210@gmail.com>
2020-11-03 14:27:23 +0300

    Scripted auto-commit on change (2020-11-03 14:27:23) by gitwatch.sh

---
+++ lab2/src/main/java/org/personalfebus/WordMapper.java
@@ -0,0 +1,33 @@
+package org.personalfebus;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+	@Override
+	protected void map(LongWritable key, Text value, Context context) throws IOException,
+	InterruptedException {
+		String line = value.toString();
+		line = line.toLowerCase();
+		StringBuilder tmp = new StringBuilder(line);
+		//1072 - 1103
+		//1105
+		for (int i = 0; i < tmp.length(); i++) {
+			int num = (int)tmp.charAt(i);
+			if (((num != 32)  && (num < 97)) || ((num > 122) && (num < 1072)) || ((num > 1103) && (num != 1105))){
+				tmp.deleteCharAt(i);
+				i--;
+			}
+		}
+		line = tmp.toString();
+		String[] words = line.split(" ");
+		for (String word : words) {
+			if (word.length() != 0) {
+				context.write(new Text(word), new IntWritable(1));
+			}
+		}
+	}
+}
