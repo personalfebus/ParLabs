954727a157deb851fdac5e8a24881fea9cad89ff
personalfebus <gleb89206535210@gmail.com>
2020-11-04 00:50:09 +0300

    Scripted auto-commit on change (2020-11-04 00:50:09) by gitwatch.sh

--- lab2/src/main/java/org/personalfebus/AirportMapper.java
+++ lab2/src/main/java/org/personalfebus/AirportMapper.java
@@ -6,7 +6,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+public class AirportMapper extends Mapper<LongWritable, Text, Text, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
@@ -14,12 +14,6 @@ public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable>
 
         String[] words = line.split(",");
         if (words[0].equals("\"Code\"") || words[0].equals("Code")) return;
-        int number = 0;
-        for (int j = 0; j < words[18].length(); j++) {
-            int digit = (int)words[18].charAt(j) - 48;
-            if (digit < 0) break;
-            number = number * 10 + digit;
-        }
-        context.write(new Text(words[14]), new IntWritable(number));
+        context.write(new Text(words[0]), new Text(words[1]));
     }
 }
