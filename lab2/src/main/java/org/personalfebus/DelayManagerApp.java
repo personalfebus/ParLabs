43b71b8c31ec41ce0aa1be9929975a21c8519fbd
personalfebus <gleb89206535210@gmail.com>
2020-11-04 00:37:07 +0300

    Scripted auto-commit on change (2020-11-04 00:37:07) by gitwatch.sh

--- lab2/src/main/java/org/personalfebus/DelayManagerApp.java
+++ lab2/src/main/java/org/personalfebus/DelayManagerApp.java
@@ -18,7 +18,7 @@ public class DelayManagerApp {
 		job.setJarByClass(DelayManagerApp.class);
 		job.setJobName("Delay Manager");
 		MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
-		MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, CallsJoinMapper.class);
+		MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
 		//FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		job.setMapperClass(DelayMapper.class);
