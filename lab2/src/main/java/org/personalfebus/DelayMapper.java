951734779d22f668519b9c6c9ecd85bfc9fa793c
personalfebus <gleb89206535210@gmail.com>
2020-11-03 19:55:19 +0300

    Scripted auto-commit on change (2020-11-03 19:55:19) by gitwatch.sh

--- lab2/src/main/java/org/personalfebus/DelayMapper.java
+++ lab2/src/main/java/org/personalfebus/DelayMapper.java
@@ -26,6 +26,7 @@ public class DelayMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 		for (int j = 0; j < words[18].length(); j++) {
 			int digit = (int)words[18].charAt(j) - 48;
 			if ((j == 0) && (digit == -3)){
+				
 				continue;
 			}
 			if (digit < 0) break;
