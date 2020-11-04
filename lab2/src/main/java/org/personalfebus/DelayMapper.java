8df125375acef16c11145f9ef225cb14a187eb49
personalfebus <gleb89206535210@gmail.com>
2020-11-04 12:56:22 +0300

    Scripted auto-commit on change (2020-11-04 12:56:22) by gitwatch.sh

--- lab2/src/main/java/org/personalfebus/DelayMapper.java
+++ lab2/src/main/java/org/personalfebus/DelayMapper.java
@@ -22,12 +22,12 @@ public class DelayMapper extends Mapper<LongWritable, Text, Text, Text> {
 
 		String[] words = line.split(",");
 		if (words[0].equals("\"YEAR\"") || words[0].equals("YEAR")) return;
-		int number = 0;
-		for (int j = 0; j < words[18].length(); j++) {
-			int digit = (int)words[18].charAt(j) - 48;
-			if (digit < 0) break;
-			number = number * 10 + digit;
-		}
+//		int number = 0;
+//		for (int j = 0; j < words[18].length(); j++) {
+//			int digit = (int)words[18].charAt(j) - 48;
+//			if (digit < 0) break;
+//			number = number * 10 + digit;
+//		}
 		context.write(new Text(words[14]), new IntWritable(number));
 	}
 }
