b8e6e0631f5abb58cf7dc0237e0b8f242cc39617
personalfebus <gleb89206535210@gmail.com>
2020-11-04 14:25:26 +0300

    Scripted auto-commit on change (2020-11-04 14:25:26) by gitwatch.sh

--- lab2/src/main/java/org/personalfebus/DelayReducer.java
+++ lab2/src/main/java/org/personalfebus/DelayReducer.java
@@ -23,7 +23,7 @@ public class DelayReducer extends Reducer<Text, Text, Text, Text> {
 			Text delayOrName = (Text)iter.next();
 			String delayOrNameStr = delayOrName.toString();
 			int firstLetterCode = (int)delayOrName.charAt(0);
-			if ((firstLetterCode > 64) && (firstLetterCode < 91)){
+			if (((firstLetterCode > 64) && (firstLetterCode < 91)) || (firstLetterCode == ){
 				airportName = delayOrNameStr;
 			} else {
 				int current_delay = 0;
