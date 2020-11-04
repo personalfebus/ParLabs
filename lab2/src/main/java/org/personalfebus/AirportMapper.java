2ef03a5235ae6fe2ed1425b94e1a7b94babeb2d1
personalfebus <gleb89206535210@gmail.com>
2020-11-04 14:35:57 +0300

    Scripted auto-commit on change (2020-11-04 14:35:57) by gitwatch.sh

--- lab2/src/main/java/org/personalfebus/AirportMapper.java
+++ lab2/src/main/java/org/personalfebus/AirportMapper.java
@@ -16,7 +16,7 @@ public class AirportMapper extends Mapper<LongWritable, Text, Text, Text> {
         String code = line.substring(1, commaPosition - 1);
         String name = line.substring(commaPosition + 2, line.length() - 1);
         if (code.equals("\"Code\"") || code.equals("Code")) return;
-        if (code.equals("10333")) System.out.println("=" + name + "=");
+        if (code.equals("10333")) System.out.println("===" + name + "===");
         context.write(new Text(code), new Text(name));
     }
 }
