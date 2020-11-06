fe033edc4e28455a1ff72c4e72d2ba9271898df9
personalfebus <gleb89206535210@gmail.com>
2020-11-06 19:30:50 +0300

    Scripted auto-commit on change (2020-11-06 19:30:50) by gitwatch.sh

--- lab2/src/main/java/org/personalfebus/DelayComparator.java
+++ lab2/src/main/java/org/personalfebus/DelayComparator.java
@@ -2,10 +2,11 @@ package org.personalfebus;
 
 import org.apache.hadoop.io.RawComparator;
 import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
 
-public class DelayComparator implements RawComparator {
+public class DelayComparator implements WritableComparator {
     @Override
     public int compare(WritableComparable a, WritableComparable b) {
-        return a.compareTo(b);
+        return super.compare(a, b);
     }
 }
