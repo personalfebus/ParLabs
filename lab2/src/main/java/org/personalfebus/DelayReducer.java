e01dce851eb856274ac79201aecffe82c754ee01
personalfebus <gleb89206535210@gmail.com>
2020-11-06 19:39:16 +0300

    Scripted auto-commit on change (2020-11-06 19:39:16) by gitwatch.sh

--- lab2/src/main/java/org/personalfebus/DelayReducer.java
+++ lab2/src/main/java/org/personalfebus/DelayReducer.java
@@ -10,7 +10,7 @@ public class DelayReducer extends Reducer<Text, Text, Text, Text> {
 	protected void reduce(Text key, Iterable<Text> values, Context context) throws
 			IOException, InterruptedException {
 		Iterator iter = values.iterator();
-		String airportName = iter.next().toString();
+		String airportName = "";
 		int count = 0;
 		int maxDelay = 0;
 		int minDelay = 0;
@@ -20,22 +20,27 @@ public class DelayReducer extends Reducer<Text, Text, Text, Text> {
 		while(iter.hasNext()){
 			Text delayOrName = (Text)iter.next();
 			String delayOrNameStr = delayOrName.toString();
-			int current_delay = 0;
-			for (int j = 0; j < delayOrNameStr.length(); j++) {
-				int digit = (int)delayOrNameStr.charAt(j) - 48;
-				if (digit < 0) break;
-				current_delay = current_delay * 10 + digit;
-			}
-			if (firstIter) {
-				maxDelay = current_delay;
-				minDelay = current_delay;
-				firstIter = false;
+			int firstLetterCode = (int)delayOrName.charAt(0);
+			if (((firstLetterCode > 64) && (firstLetterCode < 91)) || (firstLetterCode == 34)){
+				airportName = delayOrNameStr;
 			} else {
-				if (current_delay < minDelay) minDelay = current_delay;
-				if (current_delay > maxDelay) maxDelay = current_delay;
+				int current_delay = 0;
+				for (int j = 0; j < delayOrNameStr.length(); j++) {
+					int digit = (int)delayOrNameStr.charAt(j) - 48;
+					if (digit < 0) break;
+					current_delay = current_delay * 10 + digit;
+				}
+				if (firstIter) {
+					maxDelay = current_delay;
+					minDelay = current_delay;
+					firstIter = false;
+				} else {
+					if (current_delay < minDelay) minDelay = current_delay;
+					if (current_delay > maxDelay) maxDelay = current_delay;
+				}
+				sumDelay += current_delay;
+				count++;
 			}
-			sumDelay += current_delay;
-			count++;
 		}
 		int average_delay = 0;
 		if (!firstIter) {
