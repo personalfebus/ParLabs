c5aef169c3975a84111f04a82b50608891e509c9
personalfebus <gleb89206535210@gmail.com>
2020-11-13 23:11:58 +0300

    Scripted auto-commit on change (2020-11-13 23:11:58) by gitwatch.sh

--- lab2/src/main/java/org/personalfebus/AirportMapper.java
+++ lab2/src/main/java/org/personalfebus/AirportMapper.java
@@ -9,7 +9,10 @@ public class AirportMapper extends Mapper<LongWritable, Text, Text, Text> {
     public static final String COMMA_DELIMETER = ",";
     public static final String HEADER = "Code,Description";
 
-    public String
+    public String deleteAllQuotes(){
+
+    }
+    
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
