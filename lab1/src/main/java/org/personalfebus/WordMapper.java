5dcc7388960e834aca83f54f967dfe199b7237ef
personalfebus <gleb89206535210@gmail.com>
2020-11-13 23:02:44 +0300

    Scripted auto-commit on change (2020-11-13 23:02:44) by gitwatch.sh

--- lab1/src/main/java/org/personalfebus/WordMapper.java
+++ lab1/src/main/java/org/personalfebus/WordMapper.java
@@ -13,8 +13,7 @@ public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 		String line = value.toString();
 		line = line.toLowerCase();
 		StringBuilder tmp = new StringBuilder(line);
-		//1072 - 1103
-		//1105
+		
 		for (int i = 0; i < tmp.length(); i++) {
 			int num = (int)tmp.charAt(i);
 			if (((num != 32)  && (num < 97)) || ((num > 122) && (num < 1072)) || ((num > 1103) && (num != 1105))){
