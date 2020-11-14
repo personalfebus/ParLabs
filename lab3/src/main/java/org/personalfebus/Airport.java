eb724826d829820e1cf60136ddecab3030ea7d6b
personalfebus <gleb89206535210@gmail.com>
2020-11-14 21:40:52 +0300

    Scripted auto-commit on change (2020-11-14 21:40:52) by gitwatch.sh

--- lab3/src/main/java/org/personalfebus/Airport.java
+++ lab3/src/main/java/org/personalfebus/Airport.java
@@ -3,5 +3,10 @@ package org.personalfebus;
 import java.io.Serializable;
 
 public class Airport implements Serializable {
-    
+    long id;
+    String name;
+
+    public Airport(long airportId) {
+        
+    }
 }
