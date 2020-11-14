1dcf1589c2ce23ed462fb3c85c5c5c4caae36b6d
personalfebus <gleb89206535210@gmail.com>
2020-11-14 19:43:49 +0300

    Scripted auto-commit on change (2020-11-14 19:43:49) by gitwatch.sh

--- lab3/src/main/java/org/personalfebus/Airports.java
+++ lab3/src/main/java/org/personalfebus/Airports.java
@@ -6,7 +6,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 public class Airports {
 	public static void main(String[] args) throws Exception {
 		if (args.length != 3) {
-			System.err.println("Usage: WordCountApp <input path> <output path>");
+			System.err.println("Usage: Airports <input path> <output path>");
 			System.exit(-1);
 		}
 		SparkConf conf = new SparkConf().setAppName("lab5");
