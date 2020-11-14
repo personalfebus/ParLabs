bcce0c4b94a9d22f701f0e18f88a3be105eac48d
personalfebus <gleb89206535210@gmail.com>
2020-11-14 14:46:28 +0300

    Scripted auto-commit on change (2020-11-14 14:46:28) by gitwatch.sh

--- lab2/src/main/java/org/personalfebus/DelayMapper.java
+++ lab2/src/main/java/org/personalfebus/DelayMapper.java
@@ -6,17 +6,16 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class DelayMapper extends Mapper<LongWritable, Text, Text, Text> {
-	public static final String COMMA_DELIMETER = ",";
+	public static final String COMMADELIMETER = ",";
 	public static final String HEAD = "\"YEAR\"";
-	public static final int AIRPORT_ID_POSITION = 14;
-	public static final int DELAY_POSITION = 18;
 
 	@Override
 	protected void map(LongWritable key, Text value, Context context) throws IOException,
-	InterruptedException {
+			InterruptedException {
 		String line = value.toString();
-		String[] words = line.split(COMMA_DELIMETER);
-		if (words[0].equals(HEAD) || words[DELAY_POSITION].isEmpty()) return;
-		context.write(new Text(words[AIRPORT_ID_POSITION]), new Text(words[DELAY_POSITION]));
+
+		String[] words = line.split(COMMADELIMETER);
+		if (words[0].equals(HEAD) || words[18].isEmpty()) return;
+		context.write(new Text(words[14]), new Text(words[18]));
 	}
 }
