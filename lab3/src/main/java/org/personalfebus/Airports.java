dd865c1807286ef85dd237698fe6ee6ca111863e
personalfebus <gleb89206535210@gmail.com>
2020-11-14 20:54:18 +0300

    Scripted auto-commit on change (2020-11-14 20:54:18) by gitwatch.sh

--- lab3/src/main/java/org/personalfebus/Airports.java
+++ lab3/src/main/java/org/personalfebus/Airports.java
@@ -17,7 +17,8 @@ public class Airports {
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		JavaRDD<String> airportId = sc.textFile("AIRPORT_ID");
 		JavaRDD<String> ontimeSample = sc.textFile("ONTIME_SAMPLE");
-		JavaRDD<String> nameToId = airportId.flatMap(s -> s.replace("\"", ""));
+		JavaRDD<String> nameToId = airportId.flatMap(s ->
+				Arrays.stream(s.replace("\"", "")));
 //		airportId.mapToPair(s -> new Tuple2<>);
 		System.out.println("HELLO");
 	}
