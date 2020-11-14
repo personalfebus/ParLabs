b23bfce299a332681fbe4c22b72afcdb37c3df94
personalfebus <gleb89206535210@gmail.com>
2020-11-14 14:52:15 +0300

    Scripted auto-commit on change (2020-11-14 14:52:15) by gitwatch.sh

--- lab2/src/main/java/org/personalfebus/AirportMapper.java
+++ lab2/src/main/java/org/personalfebus/AirportMapper.java
@@ -6,17 +6,33 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, Text, Text> {
-    public static final String COMMADELIMETER = ",";
+    public static final String COMMA_DELIMETER = ",";
     public static final String HEADER = "Code,Description";
+
+    public String deleteAllQuotes(String line) {
+        return line.replace("\"", "");
+    }
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
         String line = value.toString();
-        if (line.equals(HEADER)) return;
-        int commaPosition = line.indexOf(COMMADELIMETER);
-        String code = line.substring(1, commaPosition - 1);
-        String name = line.substring(commaPosition + 2, line.length() - 1);
+        if (line.equals(HEADER)) {
+            return;
+        }
+
+        line = deleteAllQuotes(line);
+        int commaPosition = line.indexOf(COMMA_DELIMETER);
+        String code = line.substring(0, commaPosition - 1);
+        String name = line.substring(commaPosition + 1);
+        int aaa = 0;
+//        if (aaa < 10) {
+//            System.out.println("@@@@@@@@@@@");
+//            System.out.println(code);
+//            System.out.println(name);
+//            System.out.println("@@@@@@@@@@@");
+//            aaa++;
+//        }
         context.write(new Text(code), new Text(name));
     }
 }
