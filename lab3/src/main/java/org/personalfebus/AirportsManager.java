5018588fea6a7b791deef6de280b7c2dfe017504
personalfebus <gleb89206535210@gmail.com>
2020-11-15 23:49:42 +0300

    Scripted auto-commit on change (2020-11-15 23:49:42) by gitwatch.sh

--- lab3/src/main/java/org/personalfebus/AirportsManager.java
+++ lab3/src/main/java/org/personalfebus/AirportsManager.java
@@ -55,7 +55,15 @@ public class AirportsManager {
 		});
 		Map<Long, String> idToNameMap = idToNameRDD.collectAsMap();
 
-		JavaPairRDD<Tuple2<Long, Long>, Transfer> chunk = ontimeSample.mapToPair(s -> {
+		JavaPairRDD<Tuple2<Long, Long>, Transfer> chunk = ontimeSample.filter(s -> {
+			if (s.charAt(0) == 'C') {
+				System.out.println("@@@@@");
+				System.out.println(s);
+				System.out.println("@@@@@");
+				return false;
+			}
+			return true;
+		}).mapToPair(s -> {
 			String corrected = s.replace("\"", "");
 			String[] words = corrected.split(",");
 
