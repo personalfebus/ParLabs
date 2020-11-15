e963e8bcb5690a43587c488b5a245461712949c5
personalfebus <gleb89206535210@gmail.com>
2020-11-15 23:43:29 +0300

    Scripted auto-commit on change (2020-11-15 23:43:29) by gitwatch.sh

--- lab3/src/main/java/org/personalfebus/AirportsManager.java
+++ lab3/src/main/java/org/personalfebus/AirportsManager.java
@@ -63,7 +63,7 @@ public class AirportsManager {
 			Long destinationId = stringToNum(words[DEST_AIRPORT_ID_POSITION]);
 			Tuple2<Long, Long> origAndDestId = new Tuple2<>(originId, destinationId);
 			Transfer transfer = new Transfer(originId, destinationId);
-			
+			Long delay = stringToNum(words[DELAY_POSITION]);
 		});
 
 		System.out.println("HELLO");
