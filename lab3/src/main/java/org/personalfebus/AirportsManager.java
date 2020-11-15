7ffd491418e6df7535f7530f777f3bda7370f824
personalfebus <gleb89206535210@gmail.com>
2020-11-15 14:52:01 +0300

    Scripted auto-commit on change (2020-11-15 14:52:01) by gitwatch.sh

--- lab3/src/main/java/org/personalfebus/AirportsManager.java
+++ lab3/src/main/java/org/personalfebus/AirportsManager.java
@@ -9,7 +9,8 @@ import scala.Tuple2;
 import java.util.Arrays;
 
 public class AirportsManager {
-	public static final int AIRPORT_ID_POSITION = 14;
+	public static final int ORIGIN_AIRPORT_ID_POSITION = 14;
+	public static final int DEST_AIRPORT_ID_POSITION = 14;
 	public static final int DELAY_POSITION = 18;
 	public static final int CANCELLED_POSITION = 19;
 
