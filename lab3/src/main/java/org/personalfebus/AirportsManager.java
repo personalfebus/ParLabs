4b820dcaeee2184cac5015e65b3d7afa9984a6f1
personalfebus <gleb89206535210@gmail.com>
2020-11-15 22:52:02 +0300

    Scripted auto-commit on change (2020-11-15 22:52:02) by gitwatch.sh

--- lab3/src/main/java/org/personalfebus/AirportsManager.java
+++ lab3/src/main/java/org/personalfebus/AirportsManager.java
@@ -27,8 +27,7 @@ public class AirportsManager {
 		JavaRDD<String> airportId = sc.textFile("AIRPORT_ID");
 		JavaRDD<String> ontimeSample = sc.textFile("ONTIME_SAMPLE");
 
-		JavaPairRDD<Long, String> idToNameRDD = airportId.mapToPair(s -> s.replace());
-		Map<Long, String> idToNameMap = idToNameRDD.collectAsMap(s -> {
+		JavaPairRDD<Long, String> idToNameRDD = airportId.mapToPair(s -> {
 			String corrected = s.replace("\"", "");
 			int commaPosition = corrected.indexOf(",");
 			String code = corrected.substring(0, commaPosition);
@@ -39,6 +38,7 @@ public class AirportsManager {
 				numId = numId * 10 + digit;
 			}
 		});
+		Map<Long, String> idToNameMap = idToNameRDD.collectAsMap();
 
 //		JavaRDD<Airport> airports = airportId.flatMap(s -> {
 //					String corrected = s.replace("\"", "");
