db04b7075f15327c61d7e26f4a7e3c83dd995921
personalfebus <gleb89206535210@gmail.com>
2020-11-15 15:27:24 +0300

    Scripted auto-commit on change (2020-11-15 15:27:24) by gitwatch.sh

--- lab3/src/main/java/org/personalfebus/AirportsManager.java
+++ lab3/src/main/java/org/personalfebus/AirportsManager.java
@@ -4,7 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.rdd.util.*;
+import org.apache.spark.rdd.PairRDDFunctions.*;
 import scala.Tuple2;
 
 import java.util.Arrays;
