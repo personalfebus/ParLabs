d6b32af863fc152fd30c628406b5bf8e302eeb30
personalfebus <gleb89206535210@gmail.com>
2020-11-16 19:05:37 +0300

    Scripted auto-commit on change (2020-11-16 19:05:37) by gitwatch.sh

--- lab3/dolab.sh
+++ lab3/dolab.sh
@@ -1,5 +1,2 @@
 mvn package
-hadoop fs -rm -r output
-hadoop org.personalfebus.DelayManagerApp ONTIME_SAMPLE.csv AIRPORT_ID.csv output
-rm -r output
-hadoop fs -copyToLocal output
+spark-submit --class org.personalfebus.AirportsManager --master yarn-client --num-executors 3 target/lab3-1.0-SNAPSHOT.jar
