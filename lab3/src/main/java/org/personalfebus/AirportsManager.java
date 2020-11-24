7232a1bc868e369051fdae0224d7861653590597
personalfebus <gleb89206535210@gmail.com>
2020-11-24 19:24:30 +0300

    Scripted auto-commit on change (2020-11-24 19:24:30) by gitwatch.sh

--- lab3/src/main/java/org/personalfebus/AirportsManager.java
+++ lab3/src/main/java/org/personalfebus/AirportsManager.java
@@ -93,7 +93,6 @@ public class AirportsManager {
 			float percent = (float)transfer.getNumberOfCancelledOrDelayed() / transfer.getNumberOfFlights();
 			percent *= 100;
 			System.out.println("Percent of delayed or cancelled flights: " + percent + ";");
-			System.out.println("===================");
 		}
 		System.out.println("HELLO");
 	}
