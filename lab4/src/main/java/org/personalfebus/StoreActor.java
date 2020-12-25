057d27fa1ec9832c8c191a069b5cb1da148063a0
personalfebus <gleb89206535210@gmail.com>
2020-12-26 00:58:46 +0300

    Scripted auto-commit on change (2020-12-26 00:58:46) by gitwatch.sh

--- lab4/src/main/java/org/personalfebus/StoreActor.java
+++ lab4/src/main/java/org/personalfebus/StoreActor.java
@@ -11,7 +11,7 @@ public class StoreActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(StoreMessage.class, m -> {
-                    Map<String, Boolean> mp = store.get();
+                    Map<String, Boolean> testToResult = store.get();
                     System.out.println("receive message! "+m.toString());
                 })
                 .match(GetMessage.class, req -> sender().tell(
