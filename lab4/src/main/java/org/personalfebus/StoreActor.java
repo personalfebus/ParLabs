63c3f188acc9a7c9780120be827e0acb1af8c31f
personalfebus <gleb89206535210@gmail.com>
2020-12-26 00:57:43 +0300

    Scripted auto-commit on change (2020-12-26 00:57:43) by gitwatch.sh

--- lab4/src/main/java/org/personalfebus/StoreActor.java
+++ lab4/src/main/java/org/personalfebus/StoreActor.java
@@ -16,7 +16,7 @@ public class StoreActor extends AbstractActor {
                 })
                 .match(GetMessage.class, req -> sender().tell(
 
-                        new StoreMessage(req.getPackageId(), store.get(req.getPackageId())), self())
+//                        new StoreMessage(req.getPackageId(), store.get(req.getPackageId())), self())
                 ).build();
     }
 }
