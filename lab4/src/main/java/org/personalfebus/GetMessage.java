8f010ca087f7d96c1debf28138be5fc4a8739460
personalfebus <gleb89206535210@gmail.com>
2020-12-26 01:09:42 +0300

    Scripted auto-commit on change (2020-12-26 01:09:42) by gitwatch.sh

--- lab4/src/main/java/org/personalfebus/GetMessage.java
+++ lab4/src/main/java/org/personalfebus/GetMessage.java
@@ -2,7 +2,7 @@ package org.personalfebus;
 
 public class GetMessage {
     private int packageId;
-    
+    private String 
 
     public GetMessage(int packageId) {
         this.packageId = packageId;
