8374053ceb6fd794bed50690f1ceddf49e783766
personalfebus <gleb89206535210@gmail.com>
2020-12-26 01:37:08 +0300

    Scripted auto-commit on change (2020-12-26 01:37:08) by gitwatch.sh

--- lab4/src/main/java/org/personalfebus/RunnerActor.java
+++ lab4/src/main/java/org/personalfebus/RunnerActor.java
@@ -10,5 +10,6 @@ public class RunnerActor extends AbstractActor {
     @Override
     public AbstractActor.Receive createReceive() {
         return ReceiveBuilder.create()
+                .match()
     };
 }
