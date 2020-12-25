f4a51d06029c7befd2b3571a1496da5c7759d701
personalfebus <gleb89206535210@gmail.com>
2020-12-26 01:35:10 +0300

    Scripted auto-commit on change (2020-12-26 01:35:10) by gitwatch.sh

--- lab4/src/main/java/RunnerActor.java
+++ lab4/src/main/java/RunnerActor.java
@@ -1,2 +1,11 @@
-public class RunnerActor {
-}
+package org.personalfebus;
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RunnerActor extends AbstractActor {
+    private HashMap<Integer, HashMap<String, Boolean>> store = new HashMap<>();
+
+    @Override
+    public Receive createReceive() {
