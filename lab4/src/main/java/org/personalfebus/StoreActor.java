e241eeda2a1faddc90b18e91e3437ff94be1ccf3
personalfebus <gleb89206535210@gmail.com>
2020-12-25 16:10:04 +0300

    Scripted auto-commit on change (2020-12-25 16:10:04) by gitwatch.sh

--- lab4/src/main/java/org/personalfebus/StoreActor.java
+++ lab4/src/main/java/org/personalfebus/StoreActor.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 public class StoreActor extends AbstractActor {
     private Map<String, String> store = new HashMap<>();
+    
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
