011f88cfebefa4b59ad632790b8f38ba859ae5e0
personalfebus <gleb89206535210@gmail.com>
2020-12-26 10:26:53 +0300

    Scripted auto-commit on change (2020-12-26 10:26:53) by gitwatch.sh

--- lab4/src/main/java/org/personalfebus/TestActor.java
+++ lab4/src/main/java/org/personalfebus/TestActor.java
@@ -14,6 +14,6 @@ public class TestActor extends AbstractActor {
     public AbstractActor.Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(StoreMessage.class, m -> testRunners.tell(m, storeActor))
-                .match(GetMessage.class, m -> storeActor.tell(m, sender()));
+                .match(GetMessage.class, m -> storeActor.tell(m, sender())).build();
     }
 }
