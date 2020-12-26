02f7930b2b84f90d7ca65869f41093215a234872
personalfebus <gleb89206535210@gmail.com>
2020-12-26 09:56:47 +0300

    Scripted auto-commit on change (2020-12-26 09:56:47) by gitwatch.sh

--- lab4/src/main/java/org/personalfebus/RunnerActor.java
+++ lab4/src/main/java/org/personalfebus/RunnerActor.java
@@ -10,9 +10,9 @@ import java.util.Map;
 
 public class RunnerActor extends AbstractActor {
     private static final String JS_ENGINE_NAME = "nashorn";
-    private static final String EQUALLER_FUNCTION_NAME = "eq";
-    private static final String EQUALLER_SCRIPT = "var " + EQUALLER_FUNCTION_NAME + " = function(a, b) {return a == b}";
-    private static final Invocable equaller;
+    private static final String EQ_FUNCTION_NAME = "eq";
+    private static final String EQ_SCRIPT = "var " + EQUALLER_FUNCTION_NAME + " = function(a, b) {return a == b}";
+    private static final Invocable eq;
 
     static {
         ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName(JS_ENGINE_NAME);
