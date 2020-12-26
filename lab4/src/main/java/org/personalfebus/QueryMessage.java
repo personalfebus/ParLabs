39a773877dd466cce6439eacc4c87fabf3cb1ea6
personalfebus <gleb89206535210@gmail.com>
2020-12-26 11:47:52 +0300

    Scripted auto-commit on change (2020-12-26 11:47:52) by gitwatch.sh

--- lab4/src/main/java/org/personalfebus/QueryMessage.java
+++ lab4/src/main/java/org/personalfebus/QueryMessage.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class QueryMessage {
 
-    @JsonProperty(PROPERTY_PACKAGE_ID)          private int packageId;
+    @JsonProperty("packageId")          private int packageId;
     @JsonProperty(PROPERTY_FUNCTION_NAME)       private String functionName;
     @JsonProperty(PROPERTY_JS_SCRIPT)           private String jsScript;
     @JsonProperty(PROPERTY_TESTS)               private Test[] tests;
