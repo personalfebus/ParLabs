3a9b39d8c1c379280043cf734adcaffb4cad96bb
personalfebus <gleb89206535210@gmail.com>
2020-12-26 10:00:42 +0300

    Scripted auto-commit on change (2020-12-26 10:00:42) by gitwatch.sh

--- lab4/src/main/java/org/personalfebus/RunTestMessage.java
+++ lab4/src/main/java/org/personalfebus/RunTestMessage.java
@@ -1,4 +1,44 @@
 package org.personalfebus;
 
 public class RunTestMessage {
+    private int packageId;
+    private String jsScript;
+    private String functionName;
+    private String testName;
+    private String testResult;
+    private String parameters;
+
+    public RunTestMessage(int packageId, String jsScript, String functionName, String testName, String testResult,
+                          String parameters) {
+        this.packageId = packageId;
+        this.parameters = parameters;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.testName = testName;
+        this.testResult = testResult;
+    }
+
+    public int getPackageId() {
+        return packageId;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public String getParameters() {
+        return parameters;
+    }
+
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public String getJsScript() {
+        return jsScript;
+    }
+
+    public String getTestResult() {
+        return testResult;
+    }
 }
