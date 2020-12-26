f04090a4bd3981bfcbc9e43418d15860a071a052
personalfebus <gleb89206535210@gmail.com>
2020-12-26 09:55:26 +0300

    Scripted auto-commit on change (2020-12-26 09:55:26) by gitwatch.sh

--- lab4/src/main/java/org/personalfebus/RunnerActor.java
+++ lab4/src/main/java/org/personalfebus/RunnerActor.java
@@ -1,11 +1,16 @@
 package org.personalfebus;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
+
+import javax.script.Invocable;
 import java.util.HashMap;
 import java.util.Map;
 
 public class RunnerActor extends AbstractActor {
-    private HashMap<Integer, HashMap<String, Boolean>> store = new HashMap<>();
+    private static final String JS_ENGINE_NAME = "nashorn";
+    private static final String EQUALLER_FUNCTION_NAME = "eq";
+    private static final String EQUALLER_SCRIPT = "var " + EQUALLER_FUNCTION_NAME + " = function(a, b) {return a == b}";
+    private static final Invocable equaller;
 
     @Override
     public AbstractActor.Receive createReceive() {
