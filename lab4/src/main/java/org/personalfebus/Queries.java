39dc8e1277ed87c16456750f6655a0976dc9a0e6
personalfebus <gleb89206535210@gmail.com>
2020-12-26 11:44:37 +0300

    Scripted auto-commit on change (2020-12-26 11:44:37) by gitwatch.sh

--- lab4/src/main/java/org/personalfebus/Queries.java
+++ lab4/src/main/java/org/personalfebus/Queries.java
@@ -20,7 +20,9 @@ public class Queries {
     private static final String PROPERTY_PACKAGE_ID = "packageId";
     private static final int QUERY_TIMEOUT = 10000;
 
-    public static Route createRoute(ActorRef actorRef)
+    public static Route createRoute(ActorRef actorRef) {
+        
+    }
     static void main(String[] args) {
         ActorSystem system = ActorSystem.create("test");
         ActorRef storeActor = system.actorOf(Props.create(TestActor.class));
