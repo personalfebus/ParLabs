6ff9e775565168cfe24febc1ff6976cbf0b12119
personalfebus <gleb89206535210@gmail.com>
2020-12-26 10:21:12 +0300

    Scripted auto-commit on change (2020-12-26 10:21:12) by gitwatch.sh

--- lab4/src/main/java/org/personalfebus/TestActor.java
+++ lab4/src/main/java/org/personalfebus/TestActor.java
@@ -7,7 +7,7 @@ import akka.japi.pf.ReceiveBuilder;
 import akka.routing.RoundRobinPool;
 
 public class TestActor extends AbstractActor {
-    final 
+    static final int 
     private ActorRef storeActor = getContext().actorOf(Props.create(StoreActor.class));
     private ActorRef testRunners = getContext().actorOf(new RoundRobinPool())
     public AbstractActor.Receive createReceive() {
