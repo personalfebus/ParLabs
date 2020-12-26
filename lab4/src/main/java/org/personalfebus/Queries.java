2fb3640c16dd6d2f5af4b96a3b194b791a82fe32
personalfebus <gleb89206535210@gmail.com>
2020-12-26 10:50:26 +0300

    Scripted auto-commit on change (2020-12-26 10:50:26) by gitwatch.sh

--- lab4/src/main/java/org/personalfebus/Queries.java
+++ lab4/src/main/java/org/personalfebus/Queries.java
@@ -4,12 +4,13 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 
 public class Queries {
     static void main(String[] args) {
         ActorSystem system = ActorSystem.create("test");
         ActorRef storeActor = system.actorOf(Props.create(TestActor.class));
         final Http http = Http.get(system);
-        final 
+        final ActorMaterializer
     }
 }
