0ce9c63f772a7326388c884eabf738180dd5448d
personalfebus <gleb89206535210@gmail.com>
2020-12-26 11:47:14 +0300

    Scripted auto-commit on change (2020-12-26 11:47:14) by gitwatch.sh

--- lab4/src/main/java/org/personalfebus/QueryMessage.java
+++ lab4/src/main/java/org/personalfebus/QueryMessage.java
@@ -1,4 +1,9 @@
 package org.personalfebus;
 
 public class QueryMessage {
+
+    @JsonProperty(PROPERTY_PACKAGE_ID)          private int packageId;
+    @JsonProperty(PROPERTY_FUNCTION_NAME)       private String functionName;
+    @JsonProperty(PROPERTY_JS_SCRIPT)           private String jsScript;
+    @JsonProperty(PROPERTY_TESTS)               private Test[] tests;
 }
