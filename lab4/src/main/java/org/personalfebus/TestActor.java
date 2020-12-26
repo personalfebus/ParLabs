743d7eeecaf4dff2ce930af18b1ee4823c69c1ca
personalfebus <gleb89206535210@gmail.com>
2020-12-26 10:19:07 +0300

    Scripted auto-commit on change (2020-12-26 10:19:07) by gitwatch.sh

--- lab4/src/main/java/org/personalfebus/TestActor.java
+++ lab4/src/main/java/org/personalfebus/TestActor.java
@@ -2,11 +2,12 @@ package org.personalfebus;
 
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.actor.Props;
 import akka.japi.pf.ReceiveBuilder;
 
 public class TestActor extends AbstractActor {
     public AbstractActor.Receive createReceive() {
-        private ActorRef storeActor = getContext().actorOf()
+        private ActorRef storeActor = getContext().actorOf(Props.create())
         return ReceiveBuilder.create()
                 .match(StoreMessage.class, m -> {
 
