66e070712f3c113acadd605bd00571f7ef910895
personalfebus <gleb89206535210@gmail.com>
2020-12-26 10:51:41 +0300

    Scripted auto-commit on change (2020-12-26 10:51:41) by gitwatch.sh

--- lab4/src/main/java/org/personalfebus/Queries.java
+++ lab4/src/main/java/org/personalfebus/Queries.java
@@ -16,6 +16,7 @@ public class Queries {
         ActorRef storeActor = system.actorOf(Props.create(TestActor.class));
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> flow = create
+        final Flow<HttpRequest, HttpResponse, NotUsed> flow = ;
+        
     }
 }
