ee06798474b8e8649028bd713e3e5ae9938ddc42
personalfebus <gleb89206535210@gmail.com>
2020-12-26 10:42:59 +0300

    Scripted auto-commit on change (2020-12-26 10:42:59) by gitwatch.sh

--- lab4/src/main/java/org/personalfebus/Queries.java
+++ lab4/src/main/java/org/personalfebus/Queries.java
@@ -8,5 +8,6 @@ public class Queries {
     static void main(String[] args) {
         ActorSystem system = ActorSystem.create("test");
         ActorRef storeActor = system.actorOf(Props.create(TestActor.class));
+        final 
     }
 }
